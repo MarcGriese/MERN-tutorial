cbcb581398686bfc95f62998dd6b12369e837842
MarcGriese <marcgriese76@gmail.com>
2023-01-05 08:17:22 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
+
 const app = express();
 
 app.use(express.json());
