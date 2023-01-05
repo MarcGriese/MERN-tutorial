f481e4daa7346205445b27e2f4ec0a867a9ff808
MarcGriese <marcgriese76@gmail.com>
2023-01-05 08:16:17 +0100

    added error handling

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const { listenerCount } = require('process')
-const dotenv = require('dotenv').config()
-const {errorHandler} = require('../middleware/errorMiddleware')
-const port = process.env.PORT || 5000
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use(errorHandler)
 
-app-listenerCount(port, () => console.log(`Server has started on port ${port}`))
+app.listen(port, () => console.log(`Server has started on port ${port}`))
